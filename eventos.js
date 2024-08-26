42e87015bf75de1c8f748fd82d76770c9a619f07
marcipagani <paganimarcia3@gmail.com>
2024-08-25 21:06:50 -0300

    div

--- eventos.js
+++ eventos.js
@@ -1,3 +1,13 @@
 function saludar() {
     alert("Hola!");
   }
+
+  function saludo(event) {
+    if (!event.target.closest("click")) {
+        alert("Hola! soy el div!");
+    }
+}
+
+const container = document.getElementById("container");
+container.addEventListener("click", saludo);
+
